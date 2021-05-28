3e6cc0aaf2462a58ebda03294ea4d259bbbac218
Deepak Poudel <dipupoudel1@gmail.com>
2021-05-28 19:52:08 +0545

    Ambulance Build 1

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ if(isset($_POST['submitcontact']))
 	$email=$_POST['email'];
 	$message=$_POST['message'];
 
-	$to = "youremail@gmail.com";
+	$to = "thegreatdeepak@gmail.com";
 	$subject = "Zerotheme - Testing Contact Form";
 	$message = " Name: " . $name ."\r\n Email: " . $email . "\r\n Message:\r\n" . $message;
 	 
@@ -29,12 +29,13 @@ if(isset($_POST['submitcontact']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
+	<meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Free Bootstrap Themes designed by Zerotheme.com" />
-    <meta name="author" content="www.Zerotheme.com" />
-	<title>Health Care | Zerotheme.com</title>
+    <meta name="description" content="Ambulance Service Nepal. Providing The Best Emergency Facility" />
+    <meta name="author" content="Deepak Poudel" />
+	<title>Ambulance Care | Emergency</title>
+	
 	
 	
 	 <!-- Bootstrap Core CSS -->
@@ -87,8 +88,8 @@ if(isset($_POST['submitcontact']))
 					<a href="#" class="logo"><img src="images/logo.png" alt="Third slide"></a>
 				</div>
 				<div class="col-md-6 text-right">
-					<span>Information Service:</span></br>
-					<strong class="contact-phone"><i class="fa fa-phone"></i> 800-2345-6789</strong>
+					<span>Emergency Service:</span></br>
+					<strong class="contact-phone"><i class="fa fa-phone"></i> 984-51-67407</strong>
 				</div>
 			</div>
 		</div>
@@ -121,9 +122,9 @@ if(isset($_POST['submitcontact']))
 					<li>
                         <a class="page-scroll" href="service.html">Services</a>
                     </li>
-                    <li>
+                    <!-- <li>
                         <a class="page-scroll" href="archive.html">Blogs</a>
-                    </li>
+                    </li> -->
                     <li>
                         <a class="page-scroll" href="contact.html">Contacts</a>
                     </li>
@@ -131,6 +132,7 @@ if(isset($_POST['submitcontact']))
 			</div>
 		</div>
 	</nav>
+	
 		
 	<a id='backTop'>Back To Top</a>
 	<!-- /Back To Top -->
@@ -147,23 +149,18 @@ if(isset($_POST['submitcontact']))
 							</h2>
 						</div>
 						<div class="row">
-							<div class='embed-container maps'>
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.289259162295!2d-120.7989351!3d37.5246781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8091042b3386acd7%3A0x3b4a4cedc60363dd!2sMain+St%2C+Denair%2C+CA+95316%2C+Hoa+K%E1%BB%B3!5e0!3m2!1svi!2s!4v1434016649434" width="100%" height="450px" frameborder="0" style="border: 0"></iframe>
-							</div>
+					
 							<div class="col-md-4 box-item">
 								<h3>Contact Info</h3>
-								<span>SED UT PERSPICIATIS UNDE OMNIS ISTE NATUS ERROR SIT VOLUPTATEM ACCUSANTIUM DOLOREMQUE LAUDANTIUM, TOTAM REM APERIAM.</span><br> <br>
-								<p>JL.Kemacetan timur no.23. block.Q3<br>
-									Jakarta-Indonesia</p>
-								   <p>+6221 888 888 90 <br>
-									+6221 888 88891</p>
-								<p>info@yourdomain.com</p>
+								<span>Feel Free to Send Us Message</span><br> <br>
+								<p>Kathmandu<br>
+									Nepal</p>
+								   <p>+977 986 543 0202<br>
+									+977 9812345678</p>
+								<p>siddhantbhardwaz@gmail.com</p>
 							</div>
 							<div class="col-md-8">
 								<h3>Contact Form</h3>
-								<!--Warning-->
-								<center><?php echo $text;?></center>
-								<!---->
 								<form name="form1" method="post" action="contact.php">
 									<div class="row">
 										<div class="col-md-6">
@@ -203,6 +200,7 @@ if(isset($_POST['submitcontact']))
 		
 	</div>
 
+
 	<footer>
 		<div class="wrap-footer">
 			<div class="container">
@@ -212,34 +210,35 @@ if(isset($_POST['submitcontact']))
 						<div class="content">
 							<div class="row">
 								<div class="col-md-6">
-									<a href="#"><img src="images/22.jpg" /></a>
+									<a href="#"><img src="images/22.png" /></a>
 								</div>
 								<div class="col-md-6">
-									<a href="#"><img src="images/23.jpg" /></a>
+									<a href="#"><img src="images/23.png" /></a>
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-6">
-									<a href="#"><img src="images/24.jpg" /></a>
+									<!-- <a href="#"><img src="images/24.jpg" /></a> -->
 								</div>
 								<div class="col-md-6">
-									<a href="#"><img src="images/18.jpg" /></a>
+									<!-- <a href="#"><img src="images/18.jpg" /></a> -->
 								</div>
 							</div>
 							<div class="row">
 								<div class="col-md-6">
-									<a href="#"><img src="images/19.jpg" /></a>
+									<!-- <a href="#"><img src="images/19.jpg" /></a> -->
 								</div>
 								<div class="col-md-6">
-									<a href="#"><img src="images/20.jpg" /></a>
+									<!-- <a href="#"><img src="images/20.jpg" /></a> -->
 								</div>
 							</div>
+							
 						</div>
 					</div>
 					<div class="col-md-3 col-footer footer-2">
 						<div class="footer-heading"><h4>About Us</h4></div>
 						<div class="content">
-							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad</p>
+							<p>We are equipped with the most advanced health care equipments inside the ambulance to help you with the emergency condition and save the patient until hospital arrives.</p>
 						</div>
 					</div>
 					<div class="col-md-3 col-footer footer-3">
@@ -248,7 +247,6 @@ if(isset($_POST['submitcontact']))
 							<ul>
 								<li><a href="#"><i class="fa fa-facebook"></i> Facebook</a></li>
 								<li><a href="#"><i class="fa fa-twitter"></i> Twitter</a></li>
-								<li><a href="#"><i class="fa fa-rss"></i> RSS</a></li>
 							</ul>
 						</div>
 					</div>
@@ -259,7 +257,6 @@ if(isset($_POST['submitcontact']))
 								<li><a href="index.html"><i class="fa fa-home"></i> Home</a></li>
 								<li><a href="single.html"><i class="fa fa-users"></i> About</a></li>
 								<li><a href="service.html"><i class="fa fa-ambulance"></i> Services</a></li>
-								<li><a href="archive.html"><i class="fa fa-folder-open-o"></i>  Blogs</a></li>
 								<li><a href="contact.html"><i class="fa fa-envelope-o"></i> Contacts</a></li>
 							</ul>
 						</div>
@@ -272,7 +269,7 @@ if(isset($_POST['submitcontact']))
 		<div class="wrap-footer">
 			<div class="clearfix">
 				<div class="col-md-6 col-md-offset-3">
-					<p>Copyright @ HealthCare - Designed by <a href="https://www.Zerotheme.com" target="_blank">Zerotheme</a></p>
+					<p>Copyright @ Ambulance Care - Designed by Deepak Poudel</a></p>
 				</div>
 			</div>	
 		</div>
@@ -305,3 +302,4 @@ if(isset($_POST['submitcontact']))
 </body>
 </html>
 	
+
